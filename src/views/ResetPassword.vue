6eb024646a32dc5cdb748a25e51b3ed6c621f396
setiawanrestuaji <setiawanrestuaji123@gmail.com>
2020-09-28 13:10:02 +0700

    fs commit

---
+++ src/views/ResetPassword.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <form @submit.prevent="onSubmit()">
+      <input v-model="form.password" type="text" placeholder="Password">
+      <input v-model="form.confirmPassword" type="text" placeholder="Confrm Password">
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        password: '',
+        confirmPassword: '',
+        user_key: this.$route.query.key
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log(this.form)
+    }
+  }
+}
+</script>
